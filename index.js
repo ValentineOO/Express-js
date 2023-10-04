ec1aafa6604e3d5be855afe118b32312c5a56a97
ValentineOO <varlomxg@gmail.com>
2023-10-04 16:10:21 +0100

    express is going good so far

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
 const express = require("express");
 const path = require("path");
+const logger = require("./middleware/logger");
+const members = require("./Members");
 
 const app = express();
 
-const members = [
-  {
-    id: 1,
-    name: "John Doe",
-    email: "john@gmail.com",
-    status: "active",
-  },
+// Init middleware
 
-  {
-    id: 2,
-    name: "Bob Williams",
-    email: "bob@gmail.com",
-    status: "active",
-  },
-  {
-    id: 3,
-    name: "Shannon Jackson",
-    email: "shannon@gmail.com",
-    status: "active",
-  },
-];
+// app.use(logger);
 
-app.get("/api/members", (req, res) => {
-  res.json(members);
+// Get all members
+app.get("/api/members", (req, res) => res.json(members));
+
+// Get single member
+app.get("/api/members/:id", (req, res) => {
+  res.json(members.filter(member => member.id === parseInt(req.params.id)));
 });
 
 // Set static folder
