46258b01413bf35b4563ea46a796324801a422ba
ValentineOO <varlomxg@gmail.com>
2023-10-05 16:59:12 +0100

    express handlebar to be fixed

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const path = require("path");
+const exphbs = require("express-handlebars");
 const logger = require("./middleware/logger");
 
 const app = express();
 
 // Init middleware
-
 // app.use(logger);
+
+// Handlebars Middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
