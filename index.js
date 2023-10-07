0f62690a294ef1400d151f0b23922a6217023331
ValentineOO <varlomxg@gmail.com>
2023-10-07 14:47:48 +0100

    done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
 const logger = require("./middleware/logger");
+const members = require("./Members");
 
 const app = express();
 
@@ -9,12 +10,21 @@ const app = express();
 // app.use(logger);
 
 // Handlebars Middleware
-app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Homepage Route
+app.get("/", (req, res) =>
+  res.render("index", {
+    title: "Member app",
+    members
+
+  })
+);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
